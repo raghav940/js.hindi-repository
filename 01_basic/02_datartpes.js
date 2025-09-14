9f81801606367fd2afb958bdda062bcba85e9ddd
Raghav Dadhich <raghavdadhich04@gmail.com>
2025-09-14 14:35:12 +0530

    Add initial JavaScript files with variable declarations and console outputs

---
+++ 01_basic/02_datartpes.js
@@ -0,0 +1,7 @@
+"use strict";
+let name = "hitesh";
+let age = 22;
+let state;
+console.log(typeof hitesh);
+console.log(typeof 22);
+
--- 01_basic/test.js
+++ 01_basic/test.js
@@ -1,1 +1,10 @@
-console.log("Hello World");
+const accountId = "144553";
+let accountEmail = "hitesh@google.com";
+var accountPassword = "12345";
+accountCity = "Jaipur";
+
+accountEmail = "hca@hc.com"
+accountPassword = "21212121"
+accountCity = "Bengaluru"
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
