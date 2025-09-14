f01dbd3902ff64ec43fec4bc477a69f7827fd8e1
Raghav Dadhich <raghavdadhich04@gmail.com>
2025-09-14 15:51:30 +0530

    coversion of data

---
+++ 01_basic/03_conversioOperations.js
@@ -0,0 +1,15 @@
+ /*
+let score = 33;
+
+console.log(typeof scor);
+
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);  
+let isLoggedIn = "hitesh";
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(typeof booleanIsLoggedIn);
+*/
+let someNumber = 33;
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
