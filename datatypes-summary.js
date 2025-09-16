c7e2a7b61dfa4e4b6803442ee7d4c45f37245b89
Raghav Dadhich <raghavdadhich04@gmail.com>
2025-09-16 18:48:40 +0530

    datatype

---
+++ datatypes-summary.js
@@ -0,0 +1,34 @@
+const score = 100;
+const scoreValue = 100.4;
+const isLoggedIn = false;
+const outsideTemp = null;
+let userEmail;
+
+const id = Symbol('123');   
+const anotherId = Symbol('123');    
+
+console.log(id === anotherId);  // false
+
+const bigNumber =  1235678n
+
+const hero = ["shakttimaan", "naagraj", "doga"];
+const myObj = {
+    name: "Raghav",
+    age: 20,
+    isLoggedIn: false
+}
+console.log(typeof bigNumber);
+console.log(typeof hero);
+console.log(typeof myObj);
+console.log(typeof userEmail);
+console.log(userEmail);
+console.log(typeof outsideTemp);
+console.log(outsideTemp);
+console.log(typeof score);
+console.log(typeof scoreValue);
+console.log(typeof isLoggedIn);
+console.log(typeof id);
+console.log(id);
+console.log(typeof anotherId);
+console.log(anotherId);
+ 
