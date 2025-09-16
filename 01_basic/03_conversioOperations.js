55d784ad16e39cec5b016e6a57b1ff02f250535b
Raghav Dadhich <raghavdadhich04@gmail.com>
2025-09-16 17:20:30 +0530

    about operation

--- 01_basic/03_conversioOperations.js
+++ 01_basic/03_conversioOperations.js
@@ -11,5 +11,10 @@ console.log(typeof booleanIsLoggedIn);
 */
 let someNumber = 33;
 let stringNumber = String(someNumber);
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+let num1, num2, num3;
+num1 = num2 = num3 = 2 + 2;
+let gameCounter = 100;
+++gameCounter;
+console.log(gameCounter);
