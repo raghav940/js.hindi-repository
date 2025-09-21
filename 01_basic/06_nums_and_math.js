ec37bcd1e72531c74b486d2810553b807d294fad
Raghav Dadhich <raghavdadhich04@gmail.com>
2025-09-21 16:01:07 +0530

    about math

--- 01_basic/06_nums_and_math.js
+++ 01_basic/06_nums_and_math.js
@@ -12,3 +12,22 @@ console.log(otherNumber.toPrecision(3));
 
 const hundreds = 1000000;
 console.log(hundreds.toLocaleString('en-IN'));
+
+// +++++++++maths+++++++++++
+
+console.log(Math);
+console.log(Math.abs(-4));  
+console.log(Math.round(4.6));
+console.log(Math.ceil(4.2));  
+console.log(Math.floor(4.9));
+console.log(Math.min(0, 150, 30, 20, -8, -200)); 
+console.log(Math.max(0, 150, 30, 20, -8, -200));
+// imp part in maths 
+
+console.log(Math.random());  
+console.log((Math.random() * 10) + 1);  
+console.log(Math.floor((Math.random() * 10) + 1));
+
+const min = 10
+const max = 20
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
